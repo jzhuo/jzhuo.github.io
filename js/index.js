39d3bbfd4b03c263f646e763cbcf8d565af17983
Jason Zhuo <20866984+jzhuo@users.noreply.github.com>
2018-06-21 11:56:33 -0400

    updating js code with scroll smoothed links

--- js/index.js
+++ js/index.js
@@ -22,5 +22,17 @@ $(document).ready(function () {
             showCursor: false
         });
     });
+    
+    // *****************************************************
+    // This is the js for smoother anchor link scrolling
+    // *****************************************************
+    
+    $('a[href^="#"]').live('click',function(event){
+    event.preventDefault();
+    var target_offset = $(this.hash).offset() ? $(this.hash).offset().top : 0;
+    //change this number to create the additional off set        
+    var customoffset = 0;
+    $('html, body').animate({scrollTop:target_offset - customoffset}, 500);
+});
 
 });
