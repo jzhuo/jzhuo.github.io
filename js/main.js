f2f2f3dcab0398d8624535fc141896fd60bc6eee
Jason Zhuo <jzhuo@umass.edu>
2018-06-22 14:35:43 -0400

    file reformatting and condensing

--- js/main.js
+++ js/main.js
@@ -23,16 +23,4 @@ $(document).ready(function () {
         });
     });
     
-    // *****************************************************
-    // This is the js for smoother anchor link scrolling
-    // *****************************************************
-    
-    $('a[href^="#About"]').live('click',function(event){
-    event.preventDefault();
-    var target_offset = $(this.hash).offset() ? $(this.hash).offset().top : 0;
-    //change this number to create the additional off set        
-    var customoffset = 0;
-    $('html, body').animate({scrollTop:target_offset - customoffset}, 500);
-});
-
 });
