3377f261ba498111cadda7c1f612586bcf91bba2
Jason Zhuo <20866984+jzhuo@users.noreply.github.com>
2018-06-26 16:56:38 -0400

    adding swapActive function

--- js/main.js
+++ js/main.js
@@ -24,6 +24,31 @@ $(document).ready(function () {
     });
     
     // *****************************************************
+    // This is the js for swapping active classes
+    // *****************************************************
+
+    var sections = $('.body_page_sections'),
+        navList = navBar.find("#mb-navbar.navbar-nav");
+
+    function swapActive() {
+      top = allWindow.scrollTop();
+        $.each(sections, function(i,val) {
+          var section_page = $(this),
+              bottom = section_page.height() + section_top;
+              section_top = section_page.offset().top - 10,
+            if (top <= bottom && top >= section_top) {
+              var naItems = navList.find('li');
+              $.each(naItems ,function(i,val) {
+                var item = $(this);
+                item.find("a").removeClass("active");
+              });
+              navList.find('li [href="#' + section_page.attr('id') + '"]').addClass('active');
+            }
+        });
+
+    } // End of ChangeClass Function
+    
+    // *****************************************************
     // This is the js for toggle bamburger menu dropdown
     // *****************************************************
     
