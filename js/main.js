451923407dbbae48c9a583904cde08732b754f6b
Jason Zhuo <20866984+jzhuo@users.noreply.github.com>
2018-06-27 13:04:11 -0400

    dont need this functionality atm

--- js/main.js
+++ js/main.js
@@ -27,25 +27,25 @@ $(document).ready(function () {
     // This is the js for swapping active classes
     // *****************************************************
 
-    var sections = $('.body_page_sections'),
-        navList = navBar.find("#mb-navbar.navbar-nav");
+//     var sections = $('.body_page_sections'),
+//         navList = navBar.find("#mb-navbar.navbar-nav");
 
-    function swapActive() {
-        top = allWindow.scrollTop();
-        $.each(sections, function(i,val) {
-            var section_page = $(this);
-            var bottom = section_page.height() + section_top;
-            section_top = section_page.offset().top - 10,
-            if (top <= bottom && top >= section_top) {
-                var naItems = navList.find('li');
-                $.each(naItems ,function(i,val) {
-                    var item = $(this);
-                    item.find("a").removeClass("active");
-                });
-                navList.find('li [href="#' + section_page.attr('id') + '"]').addClass('active');
-            }
-        });
-    }
+//     function swapActive() {
+//         top = allWindow.scrollTop();
+//         $.each(sections, function(i,val) {
+//             var section_page = $(this);
+//             var bottom = section_page.height() + section_top;
+//             section_top = section_page.offset().top - 10,
+//             if (top <= bottom && top >= section_top) {
+//                 var naItems = navList.find('li');
+//                 $.each(naItems ,function(i,val) {
+//                     var item = $(this);
+//                     item.find("a").removeClass("active");
+//                 });
+//                 navList.find('li [href="#' + section_page.attr('id') + '"]').addClass('active');
+//             }
+//         });
+//     }
     
     // *****************************************************
     // This is the js for toggle bamburger menu dropdown
