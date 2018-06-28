9dd3cc468efb6700d0d2eea1ab87425cf0643a3f
Jason Zhuo <20866984+jzhuo@users.noreply.github.com>
2018-06-28 10:47:06 -0400

    rotating the order of typed text

--- js/main.js
+++ js/main.js
@@ -12,9 +12,9 @@ $(document).ready(function () {
     $(function () {
         typed.typed({
             strings: [
-                "Student.",
                 "Developer.",
-                "Researcher."
+                "Researcher.",
+                "Student."
             ],
             typeSpeed: 250,
             backDelay: 2000,
@@ -46,21 +46,6 @@ $(document).ready(function () {
 //             }
 //         });
 //     }
-    
-    // *****************************************************
-    // This is the js for toggle bamburger menu dropdown
-    // *****************************************************
-    
-//     var navLinks = navBar.find(".navbar-collapse ul li a");
-
-//     $.each( navLinks, function( i, val ) {
-
-//       var navLink = $(this);
-
-//         navLink.on('click', function (e) {
-//           navBar.find(".navbar-collapse").collapse('hide');
-//         });
-
-//     });
+   
     
 });
