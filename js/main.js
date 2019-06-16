e0a5e4014d4cdb336099485a298c43ef7ebb3ba1
root <root@jzhuoTitan.localdomain>
2019-06-16 14:05:36 -0400

    add html pages for navbar links

--- js/main.js
+++ js/main.js
@@ -23,39 +23,39 @@ $(document).ready(function () {
         });
     });
 
-    // *****************************************************
-    // This is the js for navigation bar link scrolling and swapping active classes
-    // *****************************************************
+    // // *****************************************************
+    // // This is the js for navigation bar link scrolling and swapping active classes
+    // // *****************************************************
 
-    // Add smooth scrolling to all links
-    $("a").on('click', function (event) {
+    // // Add smooth scrolling to all links
+    // $("a").on('click', function (event) {
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
+    //     // Make sure this.hash has a value before overriding default behavior
+    //     if (this.hash !== "") {
+    //         // Prevent default anchor click behavior
+    //         event.preventDefault();
 
-            // Store hash
-            var hash = this.hash;
+    //         // Store hash
+    //         var hash = this.hash;
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-            $('.hover_link').addClass('passive_link');
-            $('.active_link').removeClass('active_link');
-            $(this).removeClass("passive_link");
-            $(this).addClass("active_link");
-        } // End if
-    });
+    //         // Using jQuery's animate() method to add smooth page scroll
+    //         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    //         $('html, body').animate({
+    //             scrollTop: $(hash).offset().top
+    //         }, 800, function () {
+    //             // Add hash (#) to URL when done scrolling (default click behavior)
+    //             window.location.hash = hash;
+    //         });
+    //         $('.hover_link').addClass('passive_link');
+    //         $('.active_link').removeClass('active_link');
+    //         $(this).removeClass("passive_link");
+    //         $(this).addClass("active_link");
+    //     } // End if
+    // });
 
-    // *****************************************************
-    // This is the js for dynamic scoll nav bar anchor links
-    // *****************************************************
+    // // *****************************************************
+    // // This is the js for dynamic scoll nav bar anchor links
+    // // *****************************************************
 
     // $(window).scroll(function (event) {
     //     // defining a set list of positions
