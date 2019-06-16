2e6852485806b5663cab40beb4b18c44c236ae9c
root <root@jzhuoTitan.localdomain>
2019-06-16 18:59:26 -0400

    tweak typed parameters

--- js/main.js
+++ js/main.js
@@ -23,64 +23,64 @@ $(document).ready(function () {
         });
     });
 
-    // // *****************************************************
-    // // This is the js for navigation bar link scrolling and swapping active classes
-    // // *****************************************************
+    // *****************************************************
+    // This is the js for navigation bar link scrolling and swapping active classes
+    // *****************************************************
 
-    // // Add smooth scrolling to all links
-    // $("a").on('click', function (event) {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
 
-    //     // Make sure this.hash has a value before overriding default behavior
-    //     if (this.hash !== "") {
-    //         // Prevent default anchor click behavior
-    //         event.preventDefault();
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-    //         // Store hash
-    //         var hash = this.hash;
+            // Store hash
+            var hash = this.hash;
 
-    //         // Using jQuery's animate() method to add smooth page scroll
-    //         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    //         $('html, body').animate({
-    //             scrollTop: $(hash).offset().top
-    //         }, 800, function () {
-    //             // Add hash (#) to URL when done scrolling (default click behavior)
-    //             window.location.hash = hash;
-    //         });
-    //         $('.hover_link').addClass('passive_link');
-    //         $('.active_link').removeClass('active_link');
-    //         $(this).removeClass("passive_link");
-    //         $(this).addClass("active_link");
-    //     } // End if
-    // });
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+            $('.hover_link').addClass('passive_link');
+            $('.active_link').removeClass('active_link');
+            $(this).removeClass("passive_link");
+            $(this).addClass("active_link");
+        } // End if
+    });
 
-    // // *****************************************************
-    // // This is the js for dynamic scoll nav bar anchor links
-    // // *****************************************************
+    // *****************************************************
+    // This is the js for dynamic scoll nav bar anchor links
+    // *****************************************************
 
-    // $(window).scroll(function (event) {
-    //     // defining a set list of positions
-    //     var scroll = $(window).scrollTop();
-    //     var section1 = $('#Home').scrollTop();
-    //     var section2 = $('#Education').scrollTop();
-    //     var section3 = $('#Experience').scrollTop();
-    //     var section4 = $('#Projects').scrollTop();
-    //     var section5 = $('#Skills').scrollTop();
-    //     var section6 = $('#acknowledgements_anchor_link').scrollTop();
-    //     var section7 = $('#Contact').scrollTop();
+    $(window).scroll(function (event) {
+        // defining a set list of positions
+        var scroll = $(window).scrollTop();
+        var section1 = $('#Home').scrollTop();
+        var section2 = $('#Education').scrollTop();
+        var section3 = $('#Experience').scrollTop();
+        var section4 = $('#Projects').scrollTop();
+        var section5 = $('#Skills').scrollTop();
+        var section6 = $('#acknowledgements_anchor_link').scrollTop();
+        var section7 = $('#Contact').scrollTop();
 
-    //     if (scroll >= section1 && scroll < section2) {
-    //         $('.hover_link').addClass('passive_link');
-    //         $('.active_link').removeClass('active_link');
-    //         $("#home_link").addClass('active_link');
-    //         $("#home_link").removeClass('passive_link');
-    //     }
-    //     else if (scroll >= section2 && scroll < section3) {
-    //         $('.hover_link').addClass('passive_link');
-    //         $('.active_link').removeClass('active_link');
-    //         $("#education_link").addClass('active_link');
-    //         $("#education_link").removeClass('passive_link');
-    //     }
+        if (scroll >= section1 && scroll < section2) {
+            $('.hover_link').addClass('passive_link');
+            $('.active_link').removeClass('active_link');
+            $("#home_link").addClass('active_link');
+            $("#home_link").removeClass('passive_link');
+        }
+        else if (scroll >= section2 && scroll < section3) {
+            $('.hover_link').addClass('passive_link');
+            $('.active_link').removeClass('active_link');
+            $("#education_link").addClass('active_link');
+            $("#education_link").removeClass('passive_link');
+        }
 
-    // });
+    });
 
 });
